963b70feddea1ad292063ffb64c1b91fedc6d501
Paula Rabelo <paula_rabelo2010@hotmail.com>
2025-05-29 14:58:49 -0300

    feat: implement session management and redirection in authentication and dashboard pages

---
+++ src/app/dashboard/components/sing-out-button.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+
+const SingOutButton = () => {
+  const router = useRouter();
+  return (
+    <Button
+      onClick={() =>
+        authClient.signOut({
+          fetchOptions: {
+            onSuccess: () => {
+              router.push("/login");
+            },
+          },
+        })
+      }
+    >
+      Sair
+    </Button>
+  );
+};
+
+export default SingOutButton;
