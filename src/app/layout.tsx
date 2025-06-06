3069abf30ea79b8b7f62d78a8598642df6b9a0ad
Paula Rabelo <paula_rabelo2010@hotmail.com>
2025-06-06 15:29:18 -0300

    fix: include import in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Manrope } from "next/font/google";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 import { Toaster } from "@/components/ui/sonner";
+import { ReactQueryProvider } from "@/providers/react-query";
 
 const manrope = Manrope({
   variable: "--font-manrope",
@@ -24,7 +25,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${manrope.variable} antialiased`}>
-        <NuqsAdapter>{children}</NuqsAdapter>
+        <ReactQueryProvider>
+          <NuqsAdapter>{children}</NuqsAdapter>
+        </ReactQueryProvider>
         <Toaster position="bottom-center" richColors theme="light" />
       </body>
     </html>
